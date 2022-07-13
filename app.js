19036137ed7891b2a2089af4da545192aaa7a78a
Av-Vartan <108820901+Av-Vartan@users.noreply.github.com>
2022-07-13 11:09:13 +0300

    01-cards-sources

---
+++ app.js
@@ -0,0 +1,18 @@
+const slides = document.querySelectorAll('.slide') // Объявляем переменную slides.Получаем ее с помощью метода querySelectorAll,в строковом формате получаем каждый элемент из файла
+
+for (const slide of slides) {
+// в цикле объявили переменную slide, которая будет брать значения из переменной slides с помощью цилка <For ... of>
+    slide.addEventListener('click', ()=> {
+        clearActiveClasses () 
+// обращаемся к переменной slide и используем функцию с событием Клик
+
+        slide.classList.add('active') // обращаемсся к активному слайду
+    })
+}
+
+
+function clearActiveClasses() {
+    slides.forEach((slide)=>{
+        slide.classList.remove('active')
+    })
+} // функция для удаления класса active с помощью метода массива forEach
